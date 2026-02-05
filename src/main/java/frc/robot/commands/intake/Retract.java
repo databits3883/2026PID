65f6d49e5fdc8f49716a3fcd3b14704081c798ed
program-beep140 <ncarroll@apps.district833.org>
2026-02-05 17:33:48 -0600

    created intake retract command

---
+++ src/main/java/frc/robot/commands/intake/Retract.java
@@ -0,0 +1,51 @@
+package frc.robot.commands.intake;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.subsystems.IntakeSubsystem;
+
+public class Retract extends Command {
+    private final IntakeSubsystem intake;
+    private final long ABORT_TIME = Constants.Intake.FOUR_BAR_TIMEOUT_SEC * 1000; /* in millis */
+    private long startTime = 0;
+
+    public Retract(IntakeSubsystem intakeSubsystem) 
+    {
+        this.intake = intakeSubsystem;
+
+        addRequirements(intakeSubsystem);
+    }
+
+    @Override
+    public void initialize() {
+        intake.reverseFourBar();
+        //start a timer, We can stop after X seconds if it does not reach limit
+        startTime = System.currentTimeMillis();
+    }
+
+    @Override
+    public void execute() 
+    {
+        //Nothing to do here
+    }
+
+    @Override
+    public void end(boolean interrupted) 
+    {
+        //Stop running if told to stop
+        intake.stopFourBar();
+    }
+
+    @Override
+    public boolean isFinished() 
+    {
+        //Calculate the delta time in ms and we can abort after X milliseconds
+        long delta = System.currentTimeMillis() - startTime;
+        
+        boolean atLimit = intake.isFourBarReverseLimit();
+
+        //Stop if at limit or if we ran too long
+        return  (atLimit || (delta > ABORT_TIME));
+    }
+
+}
